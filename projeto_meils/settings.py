c51188fc3e8fd25800ae731cf701cfddb8ac275a
pratadaniel94 <danielprata@outlook.com>
2018-01-22 22:48:33 -0200

    ajuste secret key

--- projeto_meils/settings.py
+++ projeto_meils/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["meils.herokuapp.com", "localhost:8000"]
+ALLOWED_HOSTS = ["meils.herokuapp.com", "localhost"]
 
 
 # Application definition
